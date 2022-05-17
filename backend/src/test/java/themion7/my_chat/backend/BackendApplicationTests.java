7eb46ad2fe65129d0bdb2904b20d6295453c7710
themion7 <themion@naver.com>
2022-05-17 11:46:47 +0900

    first commit

---
+++ backend/src/test/java/themion7/my_chat/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package themion7.my_chat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
