5b1816f987be7b5102423fd44f1a66fe39f84e74
themion7 <themion@naver.com>
2022-05-19 17:47:38 +0900

    axios 추가

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, Method } from "axios"
+
+const baseURL = 'http://localhost:8080'
+
+export interface sendTo {
+    url: string,
+    method: Method
+}
+
+export interface Callback {
+    (res: AxiosResponse): any
+}
+export interface Fallback {
+    (response: AxiosResponse): any
+}
+
+export const send = (
+    to: sendTo, 
+    data: object, 
+    callback: Callback, 
+    fallback: Fallback
+) => {
+    const config: AxiosRequestConfig = {
+        url: to.url,
+        method: to.method,
+        data: data,
+        baseURL: baseURL
+    }
+    axios(config)
+        .then((res: AxiosResponse) => callback(res))
+        .catch((err: Error | AxiosError) => {
+            if (axios.isAxiosError(err)) {
+                console.log(err.response)
+                if (err.response) fallback(err.response)
+            } else console.log(err)
+    })
+}
