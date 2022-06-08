4a3717f9d70a08df924bcac6866c0fe91eddb569
themion7 <themion@naver.com>
2022-06-08 16:24:21 +0900

    로그아웃 기능 추가

--- backend/src/main/java/themion7/my_chat/backend/controller/TokenController.java
+++ backend/src/main/java/themion7/my_chat/backend/controller/TokenController.java
@@ -2,6 +2,10 @@ package themion7.my_chat.backend.controller;
 
 import com.auth0.jwt.JWT;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +31,17 @@ public class TokenController {
                 .getSubject()
         );
         return JwtUtils.getAccessToken(member);
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void removeAccessToken(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) {
+        Cookie refreshToken = new Cookie(JwtUtils.REFRESH_TOKEN_HEADER, null);
+        refreshToken.setMaxAge(0);
+
+        response.addCookie(refreshToken);
     }
 
 }
