e452a34eac5277be9efb72f26e95d2198f7a1cf5
themion7 <themion@naver.com>
2022-06-10 10:48:42 +0900

    테이블을 JPA로 자동 생성

--- database/ddl.sql
+++ database/ddl.sql
@@ -3,31 +3,31 @@ drop database myChat;
 create database myChat;
 use myChat;
 
-create table member (
-    id          bigint          not null auto_increment,
-    username    varchar(20)     unique not null,
-    password    varchar(255)    not null,
-    primary key (id)
-);
+-- create table member (
+--     id          bigint          not null auto_increment,
+--     username    varchar(20)     unique not null,
+--     password    varchar(255)    not null,
+--     primary key (id)
+-- );
 
-create table chatroom (
-    id          bigint          not null auto_increment,
-    title       varchar(100)    unique not null,
-    population  bigint          not null default 0,
-    primary key (id)
-);
+-- create table chatroom (
+--     id          bigint          not null auto_increment,
+--     title       varchar(100)    unique not null,
+--     population  bigint          not null default 0,
+--     primary key (id)
+-- );
 
-create table member_chatroom (
-    id          bigint          not null auto_increment,
-    member_id   bigint          not null,
-    chatroom_id bigint          not null,
-    primary key (id),
-    foreign key (member_id)
-        references member(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    foreign key (chatroom_id)
-        references chatroom(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
+-- create table member_chatroom (
+--     id          bigint          not null auto_increment,
+--     member_id   bigint          not null,
+--     chatroom_id bigint          not null,
+--     primary key (id),
+--     foreign key (member_id)
+--         references member(id)
+--         ON UPDATE CASCADE
+--         ON DELETE CASCADE,
+--     foreign key (chatroom_id)
+--         references chatroom(id)
+--         ON UPDATE CASCADE
+--         ON DELETE CASCADE
+-- );
